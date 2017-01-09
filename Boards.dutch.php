a706469a40b4a6cfcc30e08276b66b431b459600
MyMug <7french@gmail.com>
2017-01-09 11:01:00 +0100

    Add files via upload

---
+++ Boards.dutch.php
@@ -0,0 +1,35 @@
+<?php
+// Version: 2.0; Boards
+
+$txt['view_unread_category'] = 'Ongelezen berichten';
+
+// And now for the moderation 'boards'
+$txt['moderation_cat'] = 'Moderatie';
+$txt['reported_posts'] = 'Gerapporteerde berichten';
+$txt['reported_board_desc'] = '<strong>Gerapporteerd</strong> door {author}<br>in {link}<br>{time}';
+$txt['reported_open'] = array(
+	1 => '1 open report',
+	'n' => '%1$s openstaande meldingen',
+);
+$txt['reported_closed'] = array(
+	1 => '1 closed report',
+	'n' => '%1$s gesloten meldingen',
+);
+$txt['group_requests'] = 'Groeps verzoek';
+$txt['group_request_desc'] = 'Aanmelden <strong>{groupname}</strong><br>by {author}<br>{time}';
+$txt['group_req_open'] = array(
+	1 => '1 open request',
+	'n' => '%1$s openstaande verzoeken',
+);
+$txt['group_req_closed'] = array(
+	1 => '1 closed request',
+	'n' => '%1$s gesloten verzoeken',
+);
+$txt['logs_board'] = 'Logboeken';
+$txt['logs_moderation'] = 'Moderatielog';
+$txt['logs_infractions'] = 'Overtredingen log';
+$txt['error_log_count'] = array(
+	0 => 'No errors',
+	1 => '1 error',
+	'n' => '%1$s errors',
+);
