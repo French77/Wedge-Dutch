0e2f68ca6e7f35ccb43a80e219c4c5349742c265
MyMug <7french@gmail.com>
2017-03-18 09:18:41 +0100

    Update Boards.dutch.php

--- Boards.dutch.php
+++ Boards.dutch.php
@@ -8,28 +8,28 @@ $txt['moderation_cat'] = 'Moderatie';
 $txt['reported_posts'] = 'Gerapporteerde berichten';
 $txt['reported_board_desc'] = '<strong>Gerapporteerd</strong> door {author}<br>in {link}<br>{time}';
 $txt['reported_open'] = array(
-	1 => '1 open report',
+	1 => '1 openstaande melding',
 	'n' => '%1$s openstaande meldingen',
 );
 $txt['reported_closed'] = array(
-	1 => '1 closed report',
+	1 => '1 gesloten melding',
 	'n' => '%1$s gesloten meldingen',
 );
 $txt['group_requests'] = 'Groeps verzoek';
 $txt['group_request_desc'] = 'Aanmelden <strong>{groupname}</strong><br>by {author}<br>{time}';
 $txt['group_req_open'] = array(
-	1 => '1 open request',
+	1 => '1 openstaand verzoek',
 	'n' => '%1$s openstaande verzoeken',
 );
 $txt['group_req_closed'] = array(
-	1 => '1 closed request',
+	1 => '1 gesloten verzoek',
 	'n' => '%1$s gesloten verzoeken',
 );
 $txt['logs_board'] = 'Logboeken';
 $txt['logs_moderation'] = 'Moderatielog';
 $txt['logs_infractions'] = 'Overtredingen log';
 $txt['error_log_count'] = array(
-	0 => 'No errors',
-	1 => '1 error',
-	'n' => '%1$s errors',
+	0 => 'Geen fouten',
+	1 => '1 fout',
+	'n' => '%1$s fouten',
 );
